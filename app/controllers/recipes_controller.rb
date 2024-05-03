ff1f47ee2686c733c72d8f83a682c25ba7f90ab4
evangelineMavrommati <emavro19@gmail.com>
2024-05-03 16:59:23 -0500

    Add Recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,43 @@
+class RecipesController < ApplicationController
+  class RecipesController < ApplicationController
+
+    def index
+      @recipes = Recipe.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @recipe = Recipe.new
+    end
+    
+    def create
+      @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        binding.pry
+        flash[:notice] = "Recipe created successfully."
+        redirect_to recipes_path
+      else
+        flash[:error] = "Recipe could not be created."
+        render new_recipe_path
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+  
+    private
+  
+    def recipe_params
+      params[:recipe].permit(:name, :meal, :calories)
+    end
+  end
+  
+end
