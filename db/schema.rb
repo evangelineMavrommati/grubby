3e531cef6a69222a7da90cbda9276ee1d04a56f0
evangelineMavrommati <emavro19@gmail.com>
2024-05-04 11:49:52 -0500

    Add instructions to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_03_231835) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_04_164606) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_03_231835) do
     t.string "meal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "instructions"
   end
 
 end
