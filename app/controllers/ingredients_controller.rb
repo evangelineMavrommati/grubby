10ebfbd83d2e3a121953b18b7053de5843a0418a
evangelineMavrommati <emavro19@gmail.com>
2024-05-05 20:50:42 -0500

    Add IngredientsController and index template

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,17 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
