b2fb101a8d92d2ac998787a237144a1f77af02de
evangelineMavrommati <emavro19@gmail.com>
2024-05-06 13:31:37 -0500

    Start filling out Ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,7 +3,17 @@ class IngredientsController < ApplicationController
     @ingredients = Ingredient.all
   end
 
+  def new
+    @ingredient = Ingredient.new
+  end
+
   def create
+    @ingredient = Ingredient.new(ingredients_params)
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -13,5 +23,11 @@ class IngredientsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def ingredients_params
+    params.require(:ingredient).permit(:name)
   end
 end
